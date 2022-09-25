c68bf709210c2501bd3855789490ff84e6ea9118
Vlad Krokhin <3v1lh3x@gmail.com>
2022-09-25 18:56:33 +0300

    feat: close window after action

--- src/superpassgen.tsx
+++ src/superpassgen.tsx
@@ -6,31 +6,24 @@ import {
   Clipboard,
   showToast,
   Toast,
+  closeMainWindow,
+  popToRoot,
 } from '@raycast/api'
 
 import { generatePassword } from './generatePassword'
+
+type ActionType = 'copy' | 'paste'
 
 export default function Command() {
   const [masterPassword, setMasterPassword] = useState('')
   const [domain, setDomain] = useState('')
 
-  async function copyPassword() {
+  async function onActionHandler(type: ActionType) {
     try {
-      await Clipboard.copy(await generatePassword(masterPassword, domain))
-    } catch (error) {
-      if (error instanceof Error) {
-        await showToast({
-          title: 'Unhandled exception has occurred',
-          message: error.message ?? '',
-          style: Toast.Style.Failure,
-        })
-      }
-    }
-  }
-
-  async function pastePassword() {
-    try {
-      await Clipboard.paste(await generatePassword(masterPassword, domain))
+      const password = await generatePassword(masterPassword, domain)
+      await (type === 'copy' ? Clipboard.copy : Clipboard.paste)(password)
+      await popToRoot()
+      await closeMainWindow()
     } catch (error) {
       if (error instanceof Error) {
         await showToast({
@@ -46,8 +39,14 @@ export default function Command() {
     <Form
       actions={
         <ActionPanel>
-          <Action title="Paste password" onAction={pastePassword}></Action>
-          <Action title="Copy password" onAction={copyPassword}></Action>
+          <Action
+            title="Paste password"
+            onAction={() => onActionHandler('paste')}
+          ></Action>
+          <Action
+            title="Copy password"
+            onAction={() => onActionHandler('copy')}
+          ></Action>
         </ActionPanel>
       }
     >
@@ -55,7 +54,6 @@ export default function Command() {
         id="password"
         title="Master password"
         onChange={setMasterPassword}
-        storeValue={true}
       />
       <Form.TextField id="domain" title="Domain / URL" onChange={setDomain} />
     </Form>
